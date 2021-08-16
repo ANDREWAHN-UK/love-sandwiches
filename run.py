460af298e8cb5c10951532ff29e815207b4d8f64
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-08-16 10:22:22 +0000

    made sure cred.json file is added

--- run.py
+++ run.py
@@ -61,6 +61,16 @@ def update_sales_sheet(data):
     print("Sales worksheet updated succesfully. \n")
 
 
+def update_surplus_worksheet(data):
+    """
+    Updates surplus worksheet, adds new row with the list data provided
+    """
+    print("Updating surplus worksheet ...\n")
+    sales_worksheet = SHEET.worksheet("surplus")
+    sales_worksheet.append_row(data)
+    print("Surplus worksheet updated succesfully. \n")
+
+
 def calculate_surplus_data(sales_row):
     """
     Compares sales with stock to calculate a surplus.
@@ -85,7 +95,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_sheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data automation. \n")
